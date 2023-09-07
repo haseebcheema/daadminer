4fa8dce1caa01004b00898193b36abba51b33a44
haseebcheema <ihaseebtariq1@gmail.com>
2023-09-07 12:44:03 +0500

    accepting the cookies

--- daadbot/daad.py
+++ daadbot/daad.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import daadbot.constants as const
 import os
 
 
 class Daad(webdriver.Chrome):
 
-    def __init__(self, driver_path=r"C:\SeleniumDriver", teardown=False):
+    def __init__(self, driver_path=r"C:\SeleniumDriver", teardown=True):
         self.driver_path = driver_path
         self.teardown = teardown
         os.environ["PATH"] += self.driver_path
@@ -19,4 +20,8 @@ class Daad(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def accept_cookies(self):
+        cookies_btn = self.find_element(By.CLASS_NAME, 'snoop-button')
+        cookies_btn.click()
 
